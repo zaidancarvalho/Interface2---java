8156ff06904c3f19603aa96d7b4d9d26359e5e94
zaidancarvalho <felipecfernandez@hotmail.com>
2022-10-05 00:05:47 -0300

    Solution with abstract only

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Shape shape = new Circle(Color.WHITE, 3.0);
+		Shape shape2 = new Rectangle(Color.BLACK, 2.0, 3.0);
+		
+		System.out.println("Circle color: " + shape.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", shape.area()));
+		
+		System.out.println("Rectangle color: " + shape2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", shape2.area()));
+
+	}
+
+}
